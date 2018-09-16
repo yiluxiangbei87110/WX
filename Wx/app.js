955c506dea61ba43985970c58365db53884cea8f
yiluxiangbei87110 <1225459083@qq.com>
2018-09-16 21:20:54 +0800

    add

--- Wx/app.js
+++ Wx/app.js
@@ -27,6 +27,6 @@ App({
     userInfo: null,
     g_isPlayingMusic: false,
     g_currentMusicPostId: null,
-    doubanBase: "https://api.douban.com/"
+    doubanBase: "https://douban.uieee.com/"
   }
 })
